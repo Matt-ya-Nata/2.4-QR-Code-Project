67376e0234aee273e84c76219cbcb8766bcf57c2
Matt Natan <mathewsmaps@gmail.com>
2023-10-15 00:01:49 +0200

    changes to "answers"

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 
-//1. Use the inquirer npm package to get user input.
+
+import inquirer from "inquirer";
+var qr = require('qr-image');
+
 inquirer
   .prompt([
+    {"message": "Tpye in your URL: ",
+    name:"URL"}
     
-    /* Pass your questions in here */
   ])
   .then((answers) => {
-    // Use user feedback for... whatever!!
+    const url = answers.URL;
   })
   .catch((error) => {
     if (error.isTtyError) {
@@ -15,7 +19,7 @@ inquirer
       // Something else went wrong
     }
   });
-
+//1. Use the inquirer npm package to get user input.
 // 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 // 3. Create a txt file to save the user input using the native fs node module.
 
