35c46e93664309dc02532869fea27f5c0c25a384
Matt Natan <mathewsmaps@gmail.com>
2023-10-15 00:42:44 +0200

    Commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ inquirer
   .then((answers) => {
     const url = answers.URL;
     var qr_svg = qr.image(url);
-    qr_svg.pipe(fs.createWriteStream('QR_IMG_PNG'));
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
   })
   .catch((error) => {
     if (error.isTtyError) {
